60a8124d7738bb4d3e141b6a6c49879cf75583a1
David Haley <djdavidhaley@gmail.com>
2017-02-09 20:20:41 -0700

    feat: Add LightController script to control light show and change build settings to Android

---
+++ Assets/LightController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour {
+
+    [Header("Strobe (Default Mode)")]
+    public float strobeSpeed = 10f;
+    [Range(3f,8f)] public float upperIntensityBound;
+    [Range(0f, 8f)] public float lowerIntensityBound;
+
+    [Header("Light Switch (Optional Mode)")]
+    public bool lightSwitch;
+    [Range(0f, 1f)]
+    public float onOffSpeed;
+
+    [Header("Color Randomizer (Optional Mode)")]
+    public bool colorRandomizer;
+    public float changeColorInSeconds;
+
+    [Header("Rotation")]
+    public bool rotate;
+    public float rotationSpeed;
+
+    private Light lightObj;
+    private Color color;
+    
+    //State Variables
+    private bool toggleLightReady = true;
+    private bool switchColorReady = true;
+    private float onOff;
+
+    public float xAngle;
+    public float yAngle;
+    public float zAngle;
+    public float period;
+    private float time;
+
+    private void Awake()
+    {
+        lightObj = this.gameObject.GetComponent<Light>();
+    }
+
+void Update () {
+
+        RotateLight();
+
+        if (lightSwitch && toggleLightReady)
+        {
+
+            StartCoroutine("ToggleLight");
+
+        }
+        else if (!lightSwitch)
+        {
+            StartCoroutine("StrobeLight");
+        }
+
+        if (colorRandomizer && switchColorReady)
+        {
+            StartCoroutine(SwitchColor(changeColorInSeconds));
+        }
+    }
+
+    private void RotateLight()
+    {
+        time = time + (Time.deltaTime * rotationSpeed);
+        float phase = Mathf.Sin(time / period);
+        transform.localRotation = Quaternion.Euler(new Vector3(phase * xAngle, phase * yAngle, phase * zAngle));
+    }
+
+    IEnumerator ToggleLight()
+    {
+        toggleLightReady = false;
+
+        onOff = Mathf.Lerp(0f, 1f, 0f);
+
+        if (onOff == 1 || onOff == 0)
+        {
+            yield return new WaitForSeconds(onOffSpeed);
+            lightObj.enabled = !lightObj.enabled;
+            toggleLightReady = true;
+        }
+    }
+
+    IEnumerator StrobeLight()
+    {
+        lightObj.intensity = Mathf.PingPong(Time.time * strobeSpeed, upperIntensityBound);
+        yield return null;
+    }
+
+    IEnumerator SwitchColor(float changeColorInSeconds)
+    {
+        switchColorReady = false;
+        yield return new WaitForSeconds(changeColorInSeconds);
+        color = new Color(UnityEngine.Random.value, UnityEngine.Random.value, UnityEngine.Random.value);
+        lightObj.color = color;
+        switchColorReady = true;
+    }
+}
+
+

81b5352672b56c96365b4f9e5bb35bb590a6b0db
David Haley <davidhaley@protonmail.com>
2017-02-11 10:18:13 -0700

    feat: Add welcome and live performance UIs, and Typer feature

---
+++ Assets/Hello.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hello : MonoBehaviour {
+
+
+    public void OnPointerClick()
+    {
+        Debug.Log("HELLO");
+    }
+}

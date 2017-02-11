c2c5aa8e655d9bc56d7f04d0738673d9f3499cbf
David Haley <djdavidhaley@gmail.com>
2017-02-11 16:22:25 -0700

    feat: Add albedo light/dark toggle script as one emulation/method of disabling baked lighting

---
+++ Assets/Scripts/AlbedoLightDarkToggle.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlbedoLightDarkToggle : MonoBehaviour {
+
+    public Material material;
+
+    private bool lightsOn = true;
+
+    private void Awake()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+
+    public void toggleAlbedo()
+    {
+        if (lightsOn)
+        {
+            material.color = Color.black;
+            lightsOn = false;
+        }
+        else if (!lightsOn)
+        {
+            material.color = Color.white;
+            lightsOn = true;
+        }
+    }
+}

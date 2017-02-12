ab873b1a9866e626448f33f7c98f9537a3589777
David Haley <davidhaley@protonmail.com>
2017-02-12 14:21:54 -0700

    feat: Add button to play or pause music

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour {
+
+    private GameObject[] gameObjects;
+
+	// Use this for initialization
+	void Awake () {
+        gameObjects = GameObject.FindGameObjectsWithTag("StageAudioSource");
+    }
+	
+    public void ToggleAudio()
+    {
+        foreach(GameObject go in gameObjects)
+        {
+            GvrAudioSource audioSource = go.GetComponent<GvrAudioSource>();
+
+            if (!audioSource.isPlaying)
+            {
+                audioSource.Play();
+            }
+            else if (audioSource.isPlaying)
+            {
+                audioSource.Pause();
+            }
+        }
+    }
+}

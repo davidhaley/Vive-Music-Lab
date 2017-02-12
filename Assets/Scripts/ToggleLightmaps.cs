910724c92a7b16f5cac171f6eedc4076fd09be30
David Haley <djdavidhaley@gmail.com>
2017-02-12 11:15:34 -0700

    feat: Keep baked area lights for main lighting and use realtime lights for light show

---
+++ Assets/Scripts/ToggleLightmaps.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleLightmaps : MonoBehaviour {
+
+    LightmapData[] lightmap_data;
+    private bool lightmapEnabled = true;
+
+    void Start()
+    {
+        // Save reference to existing scene lightmap data.
+        lightmap_data = LightmapSettings.lightmaps;
+    }
+
+    public void Toggle()
+    {
+        if (lightmapEnabled)
+        {
+            // Disable lightmaps in scene by removing the lightmap data references
+            LightmapSettings.lightmaps = new LightmapData[] { };
+            lightmapEnabled = false;
+        }
+        else if (!lightmapEnabled)
+        {
+            // Reenable lightmap data in scene.
+            LightmapSettings.lightmaps = lightmap_data;
+            lightmapEnabled = true;
+        }
+    }
+}

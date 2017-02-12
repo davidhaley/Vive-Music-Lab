ca15bd41c175e29569a6960356daef123ee40df9
David Haley <davidhaley@protonmail.com>
2017-02-12 11:15:34 -0700

    feat: Keep baked area lights for main lighting and use realtime lights for light show

---
+++ Assets/Scripts/ToggleLightShow.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleLightShow : MonoBehaviour {
+
+    public GameObject[] mainLights;
+    public GameObject[] lightShowLights;
+    private bool mainLightsEnabled = true;
+
+    private void Awake()
+    {
+        mainLights = GameObject.FindGameObjectsWithTag("MainLights");
+        lightShowLights = GameObject.FindGameObjectsWithTag("LightShow");
+
+        foreach (GameObject light in lightShowLights)
+        {
+            light.SetActive(false);
+        }
+    }
+
+    public void ToggleLights()
+    {
+        if (mainLightsEnabled)
+        {
+            foreach (GameObject light in mainLights)
+            {
+                light.GetComponent<Light>().enabled = false;
+            }
+
+            foreach (GameObject light in lightShowLights)
+            {
+                light.SetActive(true);
+            }
+
+            mainLightsEnabled = false;
+        }
+        else if (!mainLightsEnabled)
+        {
+            foreach (GameObject light in mainLights)
+            {
+                light.GetComponent<Light>().enabled = true;
+            }
+
+            foreach (GameObject light in lightShowLights)
+            {
+                light.SetActive(false);
+            }
+
+            mainLightsEnabled = true;
+        }
+    }
+}

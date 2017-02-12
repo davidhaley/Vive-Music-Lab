2c8e284fa2cb866837e20e57516f38f23c6a6ff4
David Haley <djdavidhaley@gmail.com>
2017-02-11 18:55:57 -0700

    feat: Add UI button and fade-out upon click

---
+++ Assets/Scripts/UI/FadeButton.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeButton : MonoBehaviour {
+
+    public Image backgroundImage;
+    public MaskableGraphic foregroundImage;
+
+    public float fadeTime = 1.2f;
+    public bool faded = false;
+
+    public void Update()
+    {
+        if (faded)
+        {
+            backgroundImage.fillAmount -= 1.0f / fadeTime * Time.deltaTime;
+        }
+    }
+
+    //public delegate void MyCallback(object sender, string MyArg);
+
+    //public string DoSomeWork(string Foo, MyCallback mcb)
+    //{
+    //    mcb(this, Foo);
+    //    return Foo;
+    //}
+
+public void fadeOnClick()
+    {
+        foregroundImage.CrossFadeColor(Color.black, fadeTime * 1.2f, false, true);
+        faded = true;
+    }
+}

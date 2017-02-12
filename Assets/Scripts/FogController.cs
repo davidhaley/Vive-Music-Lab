62de45425aacd3d4876a681ecbb1fad96bbe6949
David Haley <davidhaley@protonmail.com>
2017-02-12 14:34:29 -0700

    feat: Add button to toggle fog machine

---
+++ Assets/Scripts/FogController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogController : MonoBehaviour {
+
+    private GameObject fogMachine;
+    private bool fogEnabled;
+
+	// Use this for initialization
+	void Awake() {
+        fogMachine = GameObject.FindGameObjectWithTag("FogMachine");
+        fogMachine.SetActive(false);
+        Debug.Log(fogMachine.activeSelf);
+	}
+	
+	public void Toggle()
+    {
+        if (!fogEnabled)
+        {
+            fogMachine.gameObject.SetActive(true);
+            fogEnabled = true;
+        }
+        else if (fogEnabled)
+        {
+            fogMachine.gameObject.SetActive(false);
+            fogEnabled = false;
+        }
+    }
+}

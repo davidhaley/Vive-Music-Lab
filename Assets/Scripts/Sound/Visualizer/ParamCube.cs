824a2a2113cfe4712db2bcf9c863c8ce6ffc9d21
David Haley <davidhaley@protonmail.com>
2017-02-20 15:48:19 -0700

    feat: Create audio visualizer

---
+++ Assets/Scripts/Sound/Visualizer/ParamCube.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParamCube : MonoBehaviour {
+
+    public AudioVisualizer audioVisualizer;
+    public int band;
+    public float startScale;
+    public float scaleMagnitude;
+    public bool useBuffer;
+
+    void Update () {
+        if (useBuffer)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, (audioVisualizer.bandBuffer[band] * scaleMagnitude) + startScale, transform.localScale.z);
+        }
+
+        if (!useBuffer)
+        {
+            transform.localScale = new Vector3(transform.localScale.x, (audioVisualizer.freqBands[band] * scaleMagnitude) + startScale, transform.localScale.z);
+        }
+    }
+}

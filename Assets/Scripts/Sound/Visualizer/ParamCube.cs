ffa178a4fb6a33c55864e7ebeb217b00bb2a58ca
David Haley <davidhaley@protonmail.com>
2017-02-20 19:04:37 -0700

    feat: Smooth audio visualizer, add light audio visualizer to lightshow

--- Assets/Scripts/Sound/Visualizer/ParamCube.cs
+++ Assets/Scripts/Sound/Visualizer/ParamCube.cs
@@ -10,15 +10,29 @@ public class ParamCube : MonoBehaviour {
     public float scaleMagnitude;
     public bool useBuffer;
 
+    private Material material;
+
+    private void Start()
+    {
+        material = GetComponent<MeshRenderer>().material;
+    }
+
     void Update () {
         if (useBuffer)
         {
-            transform.localScale = new Vector3(transform.localScale.x, (audioVisualizer.bandBuffer[band] * scaleMagnitude) + startScale, transform.localScale.z);
+            Modulate(audioVisualizer.audioBandBuffer);
         }
 
         if (!useBuffer)
         {
-            transform.localScale = new Vector3(transform.localScale.x, (audioVisualizer.freqBands[band] * scaleMagnitude) + startScale, transform.localScale.z);
+            Modulate(audioVisualizer.audioBand);
         }
+    }
+
+    private void Modulate(float[] audio)
+    {
+        transform.localScale = new Vector3(transform.localScale.x, (audio[band] * scaleMagnitude) + startScale, transform.localScale.z);
+        Color color = new Color(audio[band], audio[band], audio[band]);
+        material.SetColor("_EmissionColor", color);
     }
 }

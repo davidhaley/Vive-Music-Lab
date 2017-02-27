dadc8c0e01a5fb3964a049556157109a866384cc
David Haley <davidhaley@protonmail.com>
2017-02-26 17:51:13 -0700

    optimization: Remove ability for player to turn off lights, make spotlights static, not allow music to play when player leaves stage

--- Assets/Scripts/Movement/RaycastMovement.cs
+++ Assets/Scripts/Movement/RaycastMovement.cs
@@ -8,72 +8,82 @@ public class RaycastMovement : MonoBehaviour
 
 	public GameObject raycastHolder;
 	public GameObject raycastIndicator;
-    public GameObject cube;
+	public GameObject cube;
 
 	public float height = 2;
 	public float maxMoveDistance = 10;
 	
 	private bool moving = false;
 
-    RaycastHit hit;
+	RaycastHit hit;
 	float theDistance;
 
 
-    void Update () {
+	void Update () {
 
         //if (EventSystem.current.IsPointerOverGameObject())
         //{
-        //    if (raycastHolder.activeSelf == true || raycastIndicator.activeSelf == true)
-        //    {
-        //        raycastHolder.SetActive(false);
-        //        raycastIndicator.SetActive(false);
-        //    }
+        //if (raycastHolder.activeSelf == true || raycastIndicator.activeSelf == true)
+        //{
+        //    raycastHolder.SetActive(false);
+        //    raycastIndicator.SetActive(false);
+        //}
 
-        //    return;
+        //return;
         //}
         //else
         //{
-            Vector3 forwardDir = raycastHolder.transform.TransformDirection(Vector3.forward) * 100;
-            Debug.DrawRay(raycastHolder.transform.position, forwardDir, Color.blue);
+        Vector3 forwardDir = raycastHolder.transform.TransformDirection(Vector3.forward) * 100;
+			Debug.DrawRay(raycastHolder.transform.position, forwardDir, Color.blue);
 
-            if (Physics.Raycast(raycastHolder.transform.position, (forwardDir), out hit))
-            {
-                if (hit.collider.gameObject.layer == 8)
+			if (Physics.Raycast(raycastHolder.transform.position, (forwardDir), out hit))
+			{
+				if (hit.collider.gameObject.layer == 8)
+				{
+					if (raycastHolder.activeSelf == false || raycastIndicator.activeSelf == false)
+					{
+						raycastHolder.SetActive(true);
+						raycastIndicator.SetActive(true);
+					}
+
+					raycastIndicator.transform.position = hit.point;
+
+					if (hit.distance <= maxMoveDistance)
+					{
+						//Teleport(hit.point);
+					}
+				}
+                else if (hit.collider.gameObject.layer != 8)
                 {
-                    if (raycastHolder.activeSelf == false || raycastIndicator.activeSelf == false)
+                    if (raycastHolder.activeSelf == true || raycastIndicator.activeSelf == true)
                     {
-                        raycastHolder.SetActive(true);
-                        raycastIndicator.SetActive(true);
+                        raycastHolder.SetActive(false);
+                        raycastIndicator.SetActive(false);
                     }
 
-                    raycastIndicator.transform.position = hit.point;
-
-                if (hit.distance <= maxMoveDistance)
-                {
-                    //Teleport(hit.point);
+                    return;
                 }
-            }
-            }
-            else
-            {
-                if (raycastHolder.activeSelf == true || raycastIndicator.activeSelf == true)
-                {
-                    raycastHolder.SetActive(false);
-                    raycastIndicator.SetActive(false);
-                }
-            }
-        //}
-    }
+			}
+			else
+			{
+				if (raycastHolder.activeSelf == true || raycastIndicator.activeSelf == true)
+				{
+					raycastHolder.SetActive(false);
+					raycastIndicator.SetActive(false);
+				}
+			}
+		//}
+	}
 
 	public void Teleport(Vector3 location) {
-        Camera.main.transform.parent.position = new Vector3(location.x, location.y + height, location.z);
-    }
+		Camera.main.transform.parent.position = new Vector3(location.x, location.y + height, location.z);
+	}
 
-    public void OnClick()
-    {
-        if (hit.distance <= maxMoveDistance)
-        {
-            Teleport(hit.point);
-        }
-    }
+	public void OnClick()
+	{
+		if (hit.distance <= maxMoveDistance)
+		{
+			Teleport(hit.point);
+		}
+	}
 }

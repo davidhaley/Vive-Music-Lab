161519ca3e9fb5b317ffdfb12d0797dd846ae1f1
David Haley <davidhaley@protonmail.com>
2017-03-04 22:56:52 -0700

    feat: Add border hint on floor for modulation contaminator

---
+++ Assets/Scripts/ModulationContaminateHint.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ModulationContaminateHint : MonoBehaviour
+{
+    private StatusIndicator statusIndicator;
+    private bool lightsOn;
+
+    private void Awake()
+    {
+        statusIndicator = gameObject.GetComponent<StatusIndicator>();
+        statusIndicator.image.CrossFadeAlpha(0f, 0f, false);
+        statusIndicator.image.CrossFadeColor(statusIndicator.activeColor, statusIndicator.fadeTime * 1.2f, false, false);
+        lightsOn = true;
+    }
+
+    public void ToggleHidden()
+    {
+        if (!lightsOn)
+        {
+            statusIndicator.image.CrossFadeAlpha(0f, 0f, false);
+            lightsOn = true;
+        }
+        else if (lightsOn)
+        {
+            statusIndicator.image.CrossFadeAlpha(100f, 0f, false);
+            lightsOn = false;
+        }
+    }
+}

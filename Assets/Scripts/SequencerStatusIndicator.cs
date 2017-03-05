baa5a456d5c10cd6897fdcee8b60dd503be6bace
David Haley <davidhaley@protonmail.com>
2017-03-04 19:13:31 -0700

    fix: Sequencer booth status indicator would not become active when player interacted with the booth

---
+++ Assets/Scripts/SequencerStatusIndicator.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SequencerStatusIndicator : MonoBehaviour {
+
+    private Sequencer sequencer;
+    private StatusIndicator statusIndicator;
+
+    private bool toggled = false;
+
+
+    private void Awake()
+    {
+        sequencer = GameObject.FindGameObjectWithTag("SequencerCanvas").GetComponentInParent<Sequencer>();
+        statusIndicator = GetComponent<StatusIndicator>();
+    }
+
+    void Update () {
+		if (sequencer.running)
+        {
+            statusIndicator.ToggleColor();
+            toggled = true;
+        }
+	}
+}
